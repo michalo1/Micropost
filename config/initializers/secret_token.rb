576ca89b76b0d674d92dc60102fe8aea10f8a7ea
Michal Ozaniak <ozaniak.michal@gmail.com>
2014-07-26 17:33:44 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Micropost::Application.config.secret_key_base = 'ed5ffc38bda3b7b08723cdb793974b3fcd06d2894ddc98e5115a0a017f746f5544839b8501a14215ffcdf1202c9e28882bc4d985e84a4decdaf2d94d9c028db7'
